edbb3660a113a4012a71fca3d78fab86cb44b85b
Sagar Dhami <sagardhami2001@gmail.com>
2024-11-02 10:54:08 +0545

    feat: added setup for shadcn

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,10 +1,12 @@
 //This is dummy hero section
 import React from "react";
+import { Button } from "./ui/button";
 
 const HeroSection = ({ heading, sub_heading, CTAs, locale }: any) => {
   return (
     <div>
       Heading: {heading}, Sub Heading: {sub_heading}
+      <Button variant="default">Primary</Button>
     </div>
   );
 };
